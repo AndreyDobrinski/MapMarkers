fbdc4c41b97c8f96289ccc9313b521320f94424b
Andrey <naruto19d@gmail.com>
2021-01-31 13:46:51 +0200

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {HashRouter as Switch,Route} from 'react-router-dom'
+
+
+import React from 'react'
+
+
+import { routes } from './routes'
+import { AppHeader } from './cmps/AppHeader'
+import '../src/assets/styles/styles.scss'
+
+
+
+export class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <AppHeader />
+        <Switch>
+          {routes.map(route => <Route key={route.path} exact component={route.component} path={route.path} />)}
+        </Switch>
+      </main>
+    )
+  }
 }
 
-export default App;
